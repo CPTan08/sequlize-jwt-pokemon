8cbecf1725c3dcffa15c94ef8b212ad739421c79
CP Tan <spin555@hotmail.com>
2021-08-27 09:53:28 +0800

    original

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// default error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).send(err.message);
+});
+
+module.exports = app;
