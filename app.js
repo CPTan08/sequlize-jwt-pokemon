d9b823446e655239fec73cb751fc67a982ddeff0
CP Tan <spin555@hotmail.com>
2021-08-27 10:54:50 +0800

    add connect and initialise database

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const db = require("./db/models/index");
 const app = express();
 
 app.use(express.json());
+
+db.sequelize.sync();
 
 // default error handler
 app.use((err, req, res, next) => {
