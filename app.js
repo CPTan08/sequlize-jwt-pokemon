6629d9fe2a5746fc8a393ea2b73c06b18bcbb72a
CP Tan <spin555@hotmail.com>
2021-09-02 15:51:38 +0800

    add and build client

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const db = require("./db/models/index");
 const app = express();
 const pokemonRouter = require("./router/pokemon.router.js");
+
 app.use(express.json());
 
 db.sequelize.sync();
@@ -10,10 +11,23 @@ db.sequelize.sync();
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.use("/pokemon", pokemonRouter);
-//default
+// app.use("/pokemon", pokemonRouter);
+// //default
 
 const trainersRoutes = require("./router/trainers.router.js");
-app.use("/trainers", trainersRoutes);
+// app.use("/trainers", trainersRoutes);
+
+const path = require("path");
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+apiRouter.use("/pokemon", pokemonRouter);
+apiRouter.use("/trainers", trainersRoutes);
+
+// allows us to deploy both front/backend to 1 Heroku app
+app.use(express.static(path.resolve("client", "build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve("client", "build", "index.html"))
+);
 
 module.exports = app;
